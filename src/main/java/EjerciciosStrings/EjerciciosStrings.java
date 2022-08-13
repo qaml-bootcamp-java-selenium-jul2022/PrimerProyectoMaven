d43d5ea27ff9092821d96b2384da6b71dc14ac0d
Alvaro Meraz <alvmmn@gmail.com>
2022-08-13 13:24:39 -0500

    Adding Primer proyecto Maven
    Usando metodos de otro proyecto en proyecto Maven

--- src/main/java/EjerciciosStrings/EjerciciosStrings.java
+++ src/main/java/EjerciciosStrings/EjerciciosStrings.java
@@ -2,41 +2,49 @@ package EjerciciosStrings;
 
 public class EjerciciosStrings {
 
-    //Traer métodos del otro proyecto
-    public char caracterEnDeterminadoIndiceDeLaCadena (int indice, String cadena) {
+    //Traer metodos de otro proyecto
+    public char caracterEnDeterminadoIndiceDeLaCadena(int indice, String cadena) {
         //Paso 1: Validar que el índice se encuentre dentro del tamaño de la cadena
         char caracterEnIndice = '"';
         if (indice < cadena.length()) {
             //Paso 2: Asignar a la variable que retornamos el valor de charAt(indice)
             caracterEnIndice = cadena.charAt(indice);
         }
-        return  caracterEnIndice;
+        return caracterEnIndice;
     }
 
-    public int posicionDeDeterminadoCaracter (char caracter, String cadena){
-        int posicion =-1;
-        posicion=cadena.indexOf(caracter);
+    public int posicionDeDeterminadoCaracter(char caracter, String cadena){
+        int posicion = -1;
+        posicion = cadena.indexOf(caracter);
         return posicion;
     }
-
-    public char reemplazarCaracterEnDeterminadoIndiceDeLaCadena (int indice, String cadena, char caracterReemplazoDeVacios) {
-        //Paso 1: Validar que el índice se encuentre dentro del tamaño de la cadena
+    public char reemplazarCaracterEnDeterminadoIndiceDeLaCadena(int indice, String cadena, char caracterReemplazoDeVacios){
+        //Paso1: validar que el indice se encuentre dentro del tamaño de la cadena
         char caracterEnIndice = ' ';
-        cadena = cadena.replace(' ', caracterReemplazoDeVacios);
-        //System.out.println(cadena);
-        if (indice < cadena.length()) {
-            //Paso 2: Asignar a la variable que retornamos el valor de charAt(indice)
+
+        System.out.println("Cadena antes del replace: " +cadena);
+        cadena = cadena.replace(' ',caracterReemplazoDeVacios);
+
+        if (indice<cadena.length()){
+            //Paso 2: Asignar a la variable que retornamos el valor de charAt()
             caracterEnIndice = cadena.charAt(indice);
         }
-        return  caracterEnIndice;
+        System.out.println("Esta es la cadena despues de la insercion: " + cadena);
+        return caracterEnIndice;
     }
 
-    public String insertarCharEnCadena (String cadena, char caracterAInsertar, int posicion) {
-        System.out.println("El tamaño de la cadena ANTES de la insercion: " + cadena.length());
+    public String insertarCharEnCadena(String cadena, char caracterAInsertar, int posicion){
+        System.out.println("El tamaño de la cadena ANTES de la insercion: "+ cadena.length());
         StringBuilder constructorDeCadena = new StringBuilder(cadena);
-        constructorDeCadena.insert(posicion, caracterAInsertar);
-        System.out.println("El tamaño de la cadena DESPUÉS de la inserción: " + constructorDeCadena.length());
+        constructorDeCadena.insert(posicion,caracterAInsertar);
+        System.out.println("El tamaño de la cadena DESPUES de la insercion: " + constructorDeCadena.length());
         return constructorDeCadena.toString();
     }
+    public boolean metodoBooleano(String cadena1, String cadena2){
 
+        return (cadena1.startsWith(cadena2) || cadena1.endsWith(cadena2))?
+                true
+                :
+                false;
+    }
 }
