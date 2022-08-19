3ea1fc76516ca6837085ed92d677e122f6914b69
Jennifer Ramirez <jxr20920@dexcom.com>
2022-08-18 20:24:03 -0500

    Control de flujo agregando break en switch

--- src/main/java/EjerciciosControlDeFlujo/Ejercicios.java
+++ src/main/java/EjerciciosControlDeFlujo/Ejercicios.java
@@ -1,19 +1,25 @@
 package EjerciciosControlDeFlujo;
 
 public class Ejercicios {
+
     public void imprimirMensajeDiaSemana(DiaDeLaSemana dia) {
         switch (dia){
-            case LUNES:
-            case MARTES:
-            case MIERCOLES:
+            case LUNES: break;
+            case MARTES: break;
+            case MIERCOLES: break;
             case JUEVES:
                 System.out.println("Empieza el trabajo");
+                break;
             case VIERNES:
                 System.out.println("Casi fin de semana");
+                break;
             case DOMINGO:
                 System.out.println("Fin de semana!!!");
+                break;
             default:
                 System.out.println("Sabado");
+                break;
         }
     }
+
 }
--- src/test/java/EjerciciosControlDeFlujo/EjerciciosTest.java
+++ src/test/java/EjerciciosControlDeFlujo/EjerciciosTest.java
@@ -1,14 +1,20 @@
 package EjerciciosControlDeFlujo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class EjerciciosTest {
 
     @Test
     void imprimirMensajeDiaSemana() {
         Ejercicios testDiaSemana = new Ejercicios();
+
         testDiaSemana.imprimirMensajeDiaSemana(DiaDeLaSemana.SABADO);
+
+        testDiaSemana.imprimirMensajeDiaSemana(DiaDeLaSemana.JUEVES);
+
+        testDiaSemana.imprimirMensajeDiaSemana(DiaDeLaSemana.VIERNES);
+
+        testDiaSemana.imprimirMensajeDiaSemana(DiaDeLaSemana.LUNES);
     }
 }
